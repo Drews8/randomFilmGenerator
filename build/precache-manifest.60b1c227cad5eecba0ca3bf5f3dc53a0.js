8be53f8fd05a8ef35ae8a8bc57fafe03f6fe7962
Drews8 <prokhorenko.drew@gmail.com>
2019-11-27 17:24:14 +0200

    add build folder

---
+++ build/precache-manifest.60b1c227cad5eecba0ca3bf5f3dc53a0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00334f0392df53d28d4bfe8b4ea3116d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dbea512bd6054c97c0f1",
+    "url": "/static/css/main.724138bf.chunk.css"
+  },
+  {
+    "revision": "c592efb3c95974c95126",
+    "url": "/static/js/2.91f27ac7.chunk.js"
+  },
+  {
+    "revision": "dbea512bd6054c97c0f1",
+    "url": "/static/js/main.fef3dc03.chunk.js"
+  },
+  {
+    "revision": "89d6afb1b330bf722bdd",
+    "url": "/static/js/runtime-main.d72f310c.js"
+  }
+]);
