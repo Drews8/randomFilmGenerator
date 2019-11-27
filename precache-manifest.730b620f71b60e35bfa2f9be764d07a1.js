4963a1532be181ac764b23a95e296cd8cb38d4f8
Drews8 <prokhorenko.drew@gmail.com>
2019-11-27 23:44:34 +0200

    Updates

---
+++ precache-manifest.730b620f71b60e35bfa2f9be764d07a1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b23107725947873e80866fa693c50037",
+    "url": "/randomFilmGenerator/index.html"
+  },
+  {
+    "revision": "5027629eeb739e043abf",
+    "url": "/randomFilmGenerator/static/css/main.724138bf.chunk.css"
+  },
+  {
+    "revision": "da5c40d8380c2f7da111",
+    "url": "/randomFilmGenerator/static/js/2.e1cee0d3.chunk.js"
+  },
+  {
+    "revision": "5027629eeb739e043abf",
+    "url": "/randomFilmGenerator/static/js/main.9ff84da0.chunk.js"
+  },
+  {
+    "revision": "d3c0745954fc65b35197",
+    "url": "/randomFilmGenerator/static/js/runtime-main.b95e61d9.js"
+  }
+]);
